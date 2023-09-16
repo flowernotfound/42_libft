e82106f96ba89de79d4abcf538c775b479aebf3d
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2023-09-16 22:16:34 +0900

    fixed header

--- libft/srcs/ft_memcpy.c
+++ libft/srcs/ft_memcpy.c
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmitsuyo <yourLogin@student.42.fr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/16 22:02:26 by hmitsuyo          #+#    #+#             */
+/*   Updated: 2023/09/16 22:15:34 by hmitsuyo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 void	*ft_memcpy(void *buf1, const void *buf2, size_t n)
 {
-	size_t	i;
-	unsigned char	*p1;
+	size_t				i;
+	unsigned char		*p1;
 	const unsigned char	*p2;
 
 	p1 = (unsigned char *)buf1;

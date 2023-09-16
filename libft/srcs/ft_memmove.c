0fee70ffeadc7a7e279c1d0b60fae54e6a959a27
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2023-09-17 04:28:50 +0900

    finished ft_memmove.c

--- libft/srcs/ft_memmove.c
+++ libft/srcs/ft_memmove.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmitsuyo <yourLogin@student.42.fr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/17 04:27:25 by hmitsuyo          #+#    #+#             */
+/*   Updated: 2023/09/17 04:27:32 by hmitsuyo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
@@ -8,7 +20,52 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 
 	p1 = (unsigned char *)dest;
 	p2 = (const unsigned char *)src;
-	i = 0;
-	if ()
+	if (dest <= src || dest >= src + n)
+	{
+		return (ft_memcpy(dest, src, n));
+	}
+	else
+	{
+		p1 += n;
+		p2 += n;
+		i = n;
+		while (i > 0)
+		{
+			p1[i - 1] = p2[i - 1];
+			i--;
+		}
+	}
 	return (dest);
 }
+//void print_memory(char *msg, char *array, size_t n) {
+//    printf("%s: ", msg);
+//    for (size_t i = 0; i < n; ++i) {
+//        printf("%c", array[i]);
+//    }
+//    printf("\n");
+//}
+//
+//int main() {
+//    char test1[10] = "abcde";
+//    char test2[10] = "abcde";
+//    char test3[20] = "abcde";
+//    char test4[20] = "abcde";
+//
+//    print_memory("Before Test 1", test1, 5);
+//    ft_memmove(test1 + 3, test1, 2);
+//    print_memory("After Test 1", test1, 5);
+//
+//    print_memory("Before Test 2", test2, 5);
+//    ft_memmove(test2, test2 + 2, 3);
+//    print_memory("After Test 2", test2, 5);
+//
+//    print_memory("Before Test 3", test3, 5);
+//    ft_memmove(test3 + 5, test3, 5);
+//    print_memory("After Test 3", test3, 10);
+//
+//    print_memory("Before Test 4", test4, 5);
+//    ft_memmove(test4, test4 + 5, 5);
+//    print_memory("After Test 4", test4, 10);
+//
+//    return 0;
+//}
--- libft/srcs/test.c
+++ libft/srcs/test.c
@@ -1,8 +1,22 @@
+#include <stdio.h>
 #include <string.h>
 
+void test_memmove() {
+    char test1[] = "12345";
+    memmove(&test1[2], &test1[0], 2);
+    printf("Test1 result: %s (Expected: 12145)\n", test1);
+
+    char test2[] = "abcde";
+    memmove(&test2[0], &test2[2], 3);
+    printf("Test2 result: %s (Expected: cdee)\n", test2);
+    
+    char test3[] = "111111";
+    memmove(&test3[3], &test3[3], 3);
+    printf("Test3 result: %s (Expected: 111111)\n", test3);
+}
+
 int main() {
-    char str[] = "abcdef";
-    memmove(str + 2, str, 4);  // "ababef"
+    test_memmove();
     return 0;
 }
 

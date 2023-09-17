cfcdb92cda1add18d9bd62b9efa139df08a1e378
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2023-09-17 17:23:49 +0900

    updated libft.h

--- libft/srcs/ft_strlcat.c
+++ libft/srcs/ft_strlcat.c
@@ -6,10 +6,23 @@
 /*   By: hmitsuyo <yourLogin@student.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 13:10:04 by hmitsuyo          #+#    #+#             */
-/*   Updated: 2023/09/17 13:55:08 by hmitsuyo         ###   ########.fr       */
+/*   Updated: 2023/09/17 17:16:45 by hmitsuyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+size_t	ft_strlcat(char *dest, const char *src, size_t n)
+{
+	unsigned int	dest_length;
+	unsigned int	src_length;
 
+	dest_length = 0;
+	while (dest[dest_length])
+		dest_length++;
+	src_length = 0;
+	while (src[src_length])
+		src_length++;
+	
+
+}
---
+++ libft/srcs/libft.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmitsuyo <yourLogin@student.42.fr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/17 17:18:00 by hmitsuyo          #+#    #+#             */
+/*   Updated: 2023/09/17 17:22:57 by hmitsuyo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+#include <stdlib.h>
+#include <unistd.h>
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+int	ft_strlen(char *s);
+void	*ft_memset(void *dest, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+size_t	ft_strlcpy(char *dest, const char *src, size_t n);
+
+#endif

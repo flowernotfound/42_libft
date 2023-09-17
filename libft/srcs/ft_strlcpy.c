b55fdf93c0d4a9db59d86cbd3d2d9e55b8d19587
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2023-09-17 13:07:53 +0900

    finisehd ft_strlcpy.c

---
+++ libft/srcs/ft_strlcpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmitsuyo <yourLogin@student.42.fr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/17 05:05:41 by hmitsuyo          #+#    #+#             */
+/*   Updated: 2023/09/17 13:03:34 by hmitsuyo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t n)
+{
+	size_t	length;
+	size_t	i;
+
+	length = ft_strlen(src);
+	if (n == 0)
+		return (length);
+	i = 0;
+	while (i < n - 1 && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (length);
+}

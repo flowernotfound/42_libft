a08e165e4657bb76de2a894db938af283a9d2b6f
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2023-09-21 04:06:54 +0900

    finished ft_strncmp.c

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -1,29 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hmitsuyo <yourLogin@student.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/17 21:31:12 by hmitsuyo          #+#    #+#             */
-/*   Updated: 2023/09/20 14:18:41 by hmitsuyo         ###   ########.fr       */
+/*   Created: 2023/09/20 17:23:30 by hmitsuyo          #+#    #+#             */
+/*   Updated: 2023/09/21 00:14:25 by hmitsuyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strrchr(const char *str, int c)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	int	length;
+	size_t			i;
+	unsigned char	*p1;
+	unsigned char	*p2;
 
-	length = ft_strlen(str);
-	if (c == '\0')
-		return ((char *)(str + length));
-	while (length >= 0)
+	p1 = (unsigned char *)s1;
+	p2 = (unsigned char *)s2;
+	i = 0;
+	if (n == 0)
+		return (0);
+	while (i < n)
 	{
-		if (str[length] == c)
-			return ((char *)(str + length));
-		length--;
+		if (p1[i] != p2[i] || p1[i] == '\0' || p2[i] == '\0')
+			return ((int)(p1[i] - p2[i]));
+		i++;
 	}
-	return (NULL);
+	return (0);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: hmitsuyo <yourLogin@student.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 17:18:00 by hmitsuyo          #+#    #+#             */
-/*   Updated: 2023/09/18 04:23:24 by hmitsuyo         ###   ########.fr       */
+/*   Updated: 2023/09/20 17:22:43 by hmitsuyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,6 @@ size_t	ft_strlcat(char *dest, const char *src, size_t n);
 int	ft_toupper(int c);
 int	ft_tolower(int c);
 char	*ft_strchr(const char *str, int c);
-
+char	*ft_strrchr(const char *str, int c);
 
 #endif

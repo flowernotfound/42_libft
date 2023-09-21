5247b3b4e68c0e5699a5a59e8b4992c267f8a2d3
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2023-09-21 19:52:45 +0900

    finished ft_memcmp.c

---
+++ libft/ft_memcmp.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmitsuyo <yourLogin@student.42.fr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/21 19:41:45 by hmitsuyo          #+#    #+#             */
+/*   Updated: 2023/09/21 19:51:54 by hmitsuyo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	const unsigned char	p1;
+	const unsigned char	p2;
+	size_t				i;
+
+	p1 = (const unsigned char *)s1;
+	p2 = (const unsigned char *)s2;
+	i = 0;
+	while (s1[i] == s2[i] && i < n)
+		i++;
+	return ((int)(s1 - s2));
+}

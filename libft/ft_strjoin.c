bc18935cfaf924b355133d6f296dce37db8b58e7
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2023-09-23 02:27:42 +0900

    finished ft_strjoin.c

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: hmitsuyo <yourLogin@student.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 23:00:44 by hmitsuyo          #+#    #+#             */
-/*   Updated: 2023/09/22 23:01:06 by hmitsuyo         ###   ########.fr       */
+/*   Updated: 2023/09/23 02:26:13 by hmitsuyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,16 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
+	size_t	length;
+	char	*p;
 
+	if (!s1 || !s2)
+		return (NULL);
+	length = ft_strlen(s1) + ft_strlen(s2);
+	p = (char *)malloc(sizeof(char) * (length + 1));
+	if (p == NULL)
+		return (NULL);
+	ft_strlcpy(p, s1, length + 1);
+	ft_strlcat(p, s2, length + 1);
+	return (p);
 }

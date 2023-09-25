5c4e36946f68f7444844be39b6ae0f6520c7c434
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2023-09-25 11:30:34 +0900

    finished ft_lstsize.c

---
+++ libft/ft_lstsize.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstsize.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmitsuyo <yourLogin@student.42.fr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/25 11:26:06 by hmitsuyo          #+#    #+#             */
+/*   Updated: 2023/09/25 11:29:15 by hmitsuyo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_lstsize(t_list *lst)
+{
+	int		size;
+	t_list	*current;
+
+	size = 0;
+	current = lst;
+	while (lst != NULL)
+	{
+		size++;
+		current = current->next;
+	}
+	return (size);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: hmitsuyo <yourLogin@student.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 17:18:00 by hmitsuyo          #+#    #+#             */
-/*   Updated: 2023/09/25 11:13:10 by hmitsuyo         ###   ########.fr       */
+/*   Updated: 2023/09/25 11:29:51 by hmitsuyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,5 +58,6 @@ void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
+int	ft_lstsize(t_list *lst);
 
 #endif

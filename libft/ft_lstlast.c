360a57c3a6e0c46950dbdeae4ce8b3e4c67fc7a5
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2023-09-25 11:46:39 +0900

    finished ft_lstlast.c

---
+++ libft/ft_lstlast.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmitsuyo <yourLogin@student.42.fr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/25 11:31:38 by hmitsuyo          #+#    #+#             */
+/*   Updated: 2023/09/25 11:44:47 by hmitsuyo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	t_list	*current;
+
+	if (lst == NULL)
+		return (NULL);
+	current = lst;
+	while (current->next != NULL)
+		current = cuurent->next;
+	return (current);
+}

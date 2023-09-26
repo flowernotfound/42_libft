841f34f5587dbfbcb44ec89976d99acabf5272ef
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2023-09-26 10:15:06 +0900

    fixed ft_memchr.c

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: hmitsuyo <yourLogin@student.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 15:23:14 by hmitsuyo          #+#    #+#             */
-/*   Updated: 2023/09/22 05:25:20 by hmitsuyo         ###   ########.fr       */
+/*   Updated: 2023/09/26 10:13:07 by hmitsuyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,15 @@
 void	*ft_memchr(const void *buf, int c, size_t n)
 {
 	const unsigned char	*s;
+	unsigned char		ch;
 	size_t				i;
 
 	s = (const unsigned char *)buf;
+	ch = (unsigned char)c;
 	i = 0;
 	while (i < n)
 	{
-		if (*(s + i) == c)
+		if (*(s + i) == ch)
 			return ((void *)(s + i));
 		i++;
 	}
